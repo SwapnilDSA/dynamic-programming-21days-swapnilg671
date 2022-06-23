a4c3055ed7c003c0f15d2a051e6ecb16bc0fdee3
swapnilg671 <swapnilg671@gmail.com>
2022-06-23 11:34:33 +0530

    fibonacci

--- Fib.java
+++ Fib.java
@@ -1,1 +1,20 @@
-class main 
+class Solution {
+    public static int fib(int n) {
+        
+       int[] dp= new int[n+1]; 
+    dp[0]=0;
+        dp[1]=1;
+        if(n==0 || n==1) return n;
+for(int i=2; i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+        
+    }
+    public static void main(String[] args) {
+        int n = 9;
+    System.out.println(fib(n));
+    }
+    
+    
+}
